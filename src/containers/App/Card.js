d163203c2aef90ed2cc63147b67bae712f31f312
kanboo <wang.kanboo@gmail.com>
2018-05-29 23:10:19 +0800

    updata card

--- src/containers/App/Card.js
+++ src/containers/App/Card.js
@@ -11,7 +11,6 @@ export default class Card extends Component {
   //   repoUrl: PropTypes.string,
   // }
 
-
   render() {
     // const {
     //   name,
@@ -23,9 +22,27 @@ export default class Card extends Component {
 
     return (
       <div className="card">
-        card
+        <a href="aaa" className="image">
+          <img
+            src="https://images.unsplash.com/photo-1509890078470-aa80385aee41?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ff67e8117e302b0884b183c5bbb07561&auto=format&fit=crop&w=500&q=60"
+            alt="aaa"
+          />
+        </a>
+        <h3 className="major">Sed lorem</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing vehicula id nulla
+          dignissim dapibus ultrices.
+        </p>
+        <a href="aaa" className="link">
+          <FontAwesomeIcon icon="desktop" size="2x" />
+        </a>
+        <a href="aaa" className="link">
+          <FontAwesomeIcon icon={['fab', 'github']} size="2x" />
+        </a>
+        <a href="aaa" className="link">
+          <FontAwesomeIcon icon={['fab', 'wpforms']} size="2x" />
+        </a>
       </div>
     );
   }
-
 }
--- src/containers/App/DailyUI.js
+++ src/containers/App/DailyUI.js
@@ -3,32 +3,20 @@ import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import Card from './Card';
 
 export default class DailyUI extends Component {
-
-
   goNext = () => {
     const navdots = document.querySelectorAll('.nav-dot');
     navdots[1].click();
-  }
+  };
 
   render() {
     return (
       <section className="page" id="DailyUI">
         <div className="container">
-
-          <div className="sectionTitle">
-            <div className="blur" />
-            <div className="titleText">
-              <h2>Tools</h2>
-              <p>製作簡單、方便、好用的小工具可以幫助很多剛開始踏入 React 的新手快速的熟悉，像是 StartKit 就是我們想讓大家可以快速上手的所搭件的橋樑</p>
-              <button className="more">More</button>
-            </div>
-          </div>
-
           <div className="cards">
             <Card />
+            <Card />
+            <Card />
           </div>
-
-
         </div>
       </section>
     );
